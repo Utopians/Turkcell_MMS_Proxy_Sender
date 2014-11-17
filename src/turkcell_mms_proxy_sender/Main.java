da61fc2d7e5e77f18ae967eea00efe7ca2f49601
Umut Celenli <umut@celenli.com>
2014-11-17 15:51:13 +0200

    * işleme sokulmayacak dosya türleri bir array'e atıldı

--- src/turkcell_mms_proxy_sender/Main.java
+++ src/turkcell_mms_proxy_sender/Main.java
@@ -24,25 +24,37 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import java.util.Arrays.*;
 
 /*
  * @author Recep Cinet
  */
 
 public class Main {
+
+    public static final String[] IgnoredFiles = new String[] {"thumbs.db",".ds_store","smil.smil"};
+
     public static String getCharacterDataFromElement(Element e) {
-    Node child = e.getFirstChild();
-    if (child instanceof CharacterData) {
-      CharacterData cd = (CharacterData) child;
-      return cd.getData();
-    }
-    return "";
+        Node child = e.getFirstChild();
+        if (child instanceof CharacterData) {
+          CharacterData cd = (CharacterData) child;
+          return cd.getData();
+        }
+        return "";
   }
 
     // exits the application with error
     public static void exitWithError(String errorMessage){
         System.out.println(errorMessage);
         System.exit(0);
+    }
+
+    public static boolean exists(String[] arr, String targetValue) {
+        for(String s: arr){
+            if(s.equals(targetValue))
+                return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -86,12 +98,12 @@ public class Main {
             part.setContentType("application/smil;");
             port.addAttachment(part);
 
+
+            //.contains(yourChar)
             for (File file : files) {
                 String filename = file.getName();
                 int xmldenfarkli = filename.indexOf(".xml");
-                if (filename.equals("Thumbs.db") || filename.equals(".DS_Store") || filename.equals("smil.smil") || xmldenfarkli>-1) {     // xml adi args[0]
-                    // yapma bisi
-                } else {
+                if (!exists(IgnoredFiles, filename) && xmldenfarkli>-1) {
                     String fileext = filename.substring(filename.lastIndexOf(".") + 1);
                     DataHandler dh = new DataHandler(new FileDataSource(new File(file.getPath())));
                     FileTypesToSend filetype = new FileTypesToSend(fileext);
@@ -118,44 +130,48 @@ public class Main {
             } catch (IOException e) {
                 exitWithError("-900|HATA!");
             }
+
+            // could be done better with an xml parser
             String response = port.submitReq(xdoc.toString());
             int bir1 = response.indexOf("<StatusCode>") + 12;
             int bir2 = response.indexOf("</StatusCode>");
             int iki1 = response.indexOf("<StatusText>") + 12;
-            int iki2 = response.indexOf("</StatusText>");  
+            int iki2 = response.indexOf("</StatusText>");
+
             String xmlRecords = response;
             DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             InputSource is = new InputSource();
             is.setCharacterStream(new StringReader(xmlRecords));
             Document doc = db.parse(is);
             NodeList nodes = doc.getElementsByTagName("Response");
-            int uzunluk=nodes.getLength();
-            int nerden=0;
+
+            int uzunluk = nodes.getLength();
+            int nerden = 0;
             String cikis;
             if (uzunluk==0)  {
-            System.out.println("NOK," + response.substring(bir1, bir2) + "|" + response.substring(iki1, iki2));
+                System.out.println("NOK," + response.substring(bir1, bir2) + "|" + response.substring(iki1, iki2));
             }
             else
             {
-            for (int i = nerden; i < uzunluk; i++) {
-              Element element = (Element) nodes.item(i);
-              cikis="";
-              NodeList name1 = element.getElementsByTagName("Recepient");
-              Element line = (Element) name1.item(0);
-              cikis+=getCharacterDataFromElement(line) + "|";
-              NodeList name2 = element.getElementsByTagName("StatusCode");
-              line = (Element) name2.item(0);
-              //System.out.println("StatusCode: " + getCharacterDataFromElement(line));
-              cikis+=getCharacterDataFromElement(line) + "|";
-              NodeList title = element.getElementsByTagName("StatusText");
-              line = (Element) title.item(0);
-              //System.out.println("StatusText: " + getCharacterDataFromElement(line));
-              cikis+=getCharacterDataFromElement(line);
-              System.out.println(cikis);
+                for (int i = nerden; i < uzunluk; i++) {
+                  Element element = (Element) nodes.item(i);
+                  cikis="";
+                  NodeList name1 = element.getElementsByTagName("Recepient");
+                  Element line = (Element) name1.item(0);
+                  cikis+=getCharacterDataFromElement(line) + "|";
+                  NodeList name2 = element.getElementsByTagName("StatusCode");
+                  line = (Element) name2.item(0);
+                  //System.out.println("StatusCode: " + getCharacterDataFromElement(line));
+                  cikis+=getCharacterDataFromElement(line) + "|";
+                  NodeList title = element.getElementsByTagName("StatusText");
+                  line = (Element) title.item(0);
+                  //System.out.println("StatusText: " + getCharacterDataFromElement(line));
+                  cikis+=getCharacterDataFromElement(line);
+                  System.out.println(cikis);
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    }
 }
