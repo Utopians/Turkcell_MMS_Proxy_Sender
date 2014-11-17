0ff893b4de6fcc1b04dc8f0609c303fd28c067d1
Umut Celenli <umut@celenli.com>
2014-11-17 15:41:47 +0200

    * Path'ler kodun basinda olusturuldu. * Folder sonunda \ yoksa ekleniyor. (Tum OS'lar icin onerilen uygulama)

--- src/turkcell_mms_proxy_sender/Main.java
+++ src/turkcell_mms_proxy_sender/Main.java
@@ -39,112 +39,123 @@ public class Main {
     return "";
   }
 
+    // exits the application with error
+    public static void exitWithError(String errorMessage){
+        System.out.println(errorMessage);
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
-        if(args.length == 0){
-            System.out.println("Parametresiz çağırmayın.");
-            return;
+        // exit if no parameters provided
+        if(args.length == 0 || args.length < 2){
+            exitWithError("-0|java -jar Turkcell_MMS_Proxy_Sender.jar paket.xml Path+icerik_klasoru\n" +
+                    "paket.xml, icerik_klasorunun icinde olmali ve parametrede path belirtilmemeli;");
         }
 
-        String hata="";
+        // first parameter is the XML file name
+        String xmlFilename =  args[0];
+
+        // second parameter is the Folder for XML files
+        String folder = args[1];
+        if(!folder.endsWith("\\") && !folder.endsWith("//"))
+        {
+            folder = folder + "\\";
+        }
+
+        // check if directory has files
+        File dir = new File(folder);
+        File[] files = dir.listFiles();
+        if (files == null) {
+            exitWithError("-999|Klasor bulunamadi");
+        }
+
+        String xmlFilePath = new File(folder, xmlFilename).toString();
+        System.out.println("XML File " + xmlFilePath );
+        System.out.println("Smil Folder " + folder );
+
+
         try {
-            //TODO: parametre yoksa hata veriyor!
-            String icerik = args[1];
+
             SubmitReqServiceLocator locator = new SubmitReqServiceLocator();
             locator.setSubmitReqPortEndpointAddress("http://stargate.turkcell.com.tr/Mmsgroup/services/SubmitReq");
             SubmitReqBindingStub port = (SubmitReqBindingStub) locator.getSubmitReqPort();
-            File dir = new File(icerik); //windows ise  + "//" ekle;
             AttachmentPart part;
-            File[] files = dir.listFiles();
-            String smilpath = icerik + "smil.smil";
-            part = new AttachmentPart(new DataHandler(
-                    new FileDataSource(new File(smilpath))));
-            part.addMimeHeader("Content-Disposition",
-                    "attachment; filename=smil.smil");
+            String smilpath = folder + "smil.smil";
+            part = new AttachmentPart(new DataHandler(new FileDataSource(new File(smilpath))));
+            part.addMimeHeader("Content-Disposition","attachment; filename=smil.smil");
             part.setContentType("application/smil;");
             port.addAttachment(part);
-            if (files == null) {
-                hata = "-999|Klasor bulunamadi";
-                //System.out.println("Klasör bulunamadı!");
-            } else {
-                for (File file : files) {
-                    String filename = file.getName();
-                    int xmldenfarkli = filename.indexOf(".xml");
-                    if (filename.equals("Thumbs.db") || filename.equals(".DS_Store") || filename.equals("smil.smil") || xmldenfarkli>-1) {     // xml adi args[0]
-                        // yapma bisi
-                    } else {
-                        String fileext = filename.substring(filename.lastIndexOf(".") + 1);
-                        DataHandler dh = new DataHandler(new FileDataSource(new File(file.getPath())));
-                        FileTypesToSend filetype = new FileTypesToSend(fileext);
-                        part = new AttachmentPart(dh);
-                        part.addMimeHeader("Content-Disposition",
-                                "attachment; filename=" + filename);
-                        if (fileext.equalsIgnoreCase("txt")) {
-                            String str = new String(part.getContent().toString().getBytes(), "UTF-8");
-                            part.setContent(str, filetype.ContentType);
-                        }
-                        // if (!filetype.ContentType.isEmpty()) {
-                        part.setContentType(filetype.ContentType);
-                        // }
-                        port.addAttachment(part);
+
+            for (File file : files) {
+                String filename = file.getName();
+                int xmldenfarkli = filename.indexOf(".xml");
+                if (filename.equals("Thumbs.db") || filename.equals(".DS_Store") || filename.equals("smil.smil") || xmldenfarkli>-1) {     // xml adi args[0]
+                    // yapma bisi
+                } else {
+                    String fileext = filename.substring(filename.lastIndexOf(".") + 1);
+                    DataHandler dh = new DataHandler(new FileDataSource(new File(file.getPath())));
+                    FileTypesToSend filetype = new FileTypesToSend(fileext);
+                    part = new AttachmentPart(dh);
+                    part.addMimeHeader("Content-Disposition","attachment; filename=" + filename);
+                    if (fileext.equalsIgnoreCase("txt")) {
+                        String str = new String(part.getContent().toString().getBytes(), "UTF-8");
+                        part.setContent(str, filetype.ContentType);
                     }
+                    part.setContentType(filetype.ContentType);
+                    port.addAttachment(part);
                 }
             }
+
             String xdoc = "";
             try {
                 DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-                InputStream inputStream = new FileInputStream(new File(icerik + args[0])); // xml adi
+                InputStream inputStream = new FileInputStream(xmlFilePath); // xml adi
                 org.w3c.dom.Document doc = documentBuilderFactory.newDocumentBuilder().parse(inputStream);
                 StringWriter stw = new StringWriter();
                 Transformer serializer = TransformerFactory.newInstance().newTransformer();
                 serializer.transform(new DOMSource(doc), new StreamResult(stw));
                 xdoc = xdoc + stw.toString();
             } catch (IOException e) {
-                if (hata!=""){
-                System.out.println(hata);
-                System.exit(0);
-                }
-                if (hata==""){
-                System.out.println("-900|HATA!");
-                }
+                exitWithError("-900|HATA!");
             }
             String response = port.submitReq(xdoc.toString());
             int bir1 = response.indexOf("<StatusCode>") + 12;
             int bir2 = response.indexOf("</StatusCode>");
             int iki1 = response.indexOf("<StatusText>") + 12;
             int iki2 = response.indexOf("</StatusText>");  
-             String xmlRecords = response;
-    DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-    InputSource is = new InputSource();
-    is.setCharacterStream(new StringReader(xmlRecords));
-    Document doc = db.parse(is);
-    NodeList nodes = doc.getElementsByTagName("Response");
-    int uzunluk=nodes.getLength();
-    int nerden=0;
-    String cikis;
-    if (uzunluk==0)  {
-    System.out.println("NOK," + response.substring(bir1, bir2) + "|" + response.substring(iki1, iki2));
-    }
-    else
-    {
-    for (int i = nerden; i < uzunluk; i++) {
-      Element element = (Element) nodes.item(i);
-      cikis="";
-      NodeList name1 = element.getElementsByTagName("Recepient");
-      Element line = (Element) name1.item(0);
-      cikis+=getCharacterDataFromElement(line) + "|";
-      NodeList name2 = element.getElementsByTagName("StatusCode");
-      line = (Element) name2.item(0);
-      //System.out.println("StatusCode: " + getCharacterDataFromElement(line));
-      cikis+=getCharacterDataFromElement(line) + "|";
-      NodeList title = element.getElementsByTagName("StatusText");
-      line = (Element) title.item(0);
-      //System.out.println("StatusText: " + getCharacterDataFromElement(line));
-      cikis+=getCharacterDataFromElement(line);
-      System.out.println(cikis);
-    }
-    }
-        } catch (Exception e) {
-            e.printStackTrace();
+            String xmlRecords = response;
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(xmlRecords));
+            Document doc = db.parse(is);
+            NodeList nodes = doc.getElementsByTagName("Response");
+            int uzunluk=nodes.getLength();
+            int nerden=0;
+            String cikis;
+            if (uzunluk==0)  {
+            System.out.println("NOK," + response.substring(bir1, bir2) + "|" + response.substring(iki1, iki2));
+            }
+            else
+            {
+            for (int i = nerden; i < uzunluk; i++) {
+              Element element = (Element) nodes.item(i);
+              cikis="";
+              NodeList name1 = element.getElementsByTagName("Recepient");
+              Element line = (Element) name1.item(0);
+              cikis+=getCharacterDataFromElement(line) + "|";
+              NodeList name2 = element.getElementsByTagName("StatusCode");
+              line = (Element) name2.item(0);
+              //System.out.println("StatusCode: " + getCharacterDataFromElement(line));
+              cikis+=getCharacterDataFromElement(line) + "|";
+              NodeList title = element.getElementsByTagName("StatusText");
+              line = (Element) title.item(0);
+              //System.out.println("StatusText: " + getCharacterDataFromElement(line));
+              cikis+=getCharacterDataFromElement(line);
+              System.out.println(cikis);
+            }
         }
-    }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 }
